47876c476e04336b3cee02bc2ee5eeedcb48f81d
Ihor Stefurak <opryshok@gmail.com>
2022-07-14 16:55:22 +0300

    content structure update

--- src/api/sector/services/sector.js
+++ src/api/sector/services/sector.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category service.
+ * sector service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::category.category');
+module.exports = createCoreService('api::sector.sector');
